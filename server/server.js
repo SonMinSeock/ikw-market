ee5284ebb62e43ddc47a37919e888383e3647f86
phj <aaagowns@gmail.com>
2023-09-25 16:22:47 +0900

    프로젝트 세팅

---
+++ server/server.js
@@ -0,0 +1,10 @@
+import express from "express";
+import test from "./Router/test.js";
+const app = express();
+
+app.use("/api", test);
+
+const port = 3002; //node 서버가 사용할 포트 번호, 리액트의 포트번호(3000)와 충돌하지 않게 다른 번호로 할당
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
