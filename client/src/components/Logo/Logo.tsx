3a13159b9c31f6a47122c222d4730450b258ee00
phj <aaagowns@gmail.com>
2023-09-26 13:12:26 +0900

    Chore : 라우터 수정
    메인페이지 path : /main -> / 으로 수정
    
    로고 클릭시 메인페이지 이동

--- client/src/components/Logo/Logo.tsx
+++ client/src/components/Logo/Logo.tsx
@@ -1,12 +1,15 @@
 import React from "react";
 import * as S from "./Logo.style";
 import LogoImage from "../../assets/logo/logo.png";
+import { Link } from "react-router-dom";
 const Logo = () => {
   return (
-    <S.LogoBox>
-      <img src={LogoImage} alt="로고 이미지" />
-      <h2>경운마켓</h2>
-    </S.LogoBox>
+    <Link to={"/"}>
+      <S.LogoBox>
+        <img src={LogoImage} alt="로고 이미지" />
+        <h2>경운마켓</h2>
+      </S.LogoBox>
+    </Link>
   );
 };
 
