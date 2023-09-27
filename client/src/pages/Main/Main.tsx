e193219a2df08af4dc186934624a2a8ea0b744df
phj <aaagowns@gmail.com>
2023-09-27 15:51:07 +0900

    Chore : 페이지 및 컴포넌트 명 수정
    /pages
    
    Products -> ProdcutDetail로 하려고 했으나 주소길이가 너무 길어짐 그래서 그냥 변경 X
    
    /components
    제품들 보여주는 ProductList 컴포넌트 생성
    
    /components/atom
    쪼갤수없는 가장 작은 컴포넌트들
    Nav, Logo, Product 따로 정리
    
    Logo
    로그인 창에서 반응형으로 사라지지 않게 수정

--- client/src/pages/Main/Main.tsx
+++ client/src/pages/Main/Main.tsx
@@ -1,13 +1,13 @@
 import React from "react";
 import * as S from "./Main.style";
 
-import Products from "../../components/ProductList/ProductList";
+import ProductList from "../../components/ProductList/ProductList";
 
 const Main = () => {
   return (
     <S.MainLayout>
       <S.MainTitle>최근 중고거래 매물</S.MainTitle>
-      <Products />
+      <ProductList />
     </S.MainLayout>
   );
 };
