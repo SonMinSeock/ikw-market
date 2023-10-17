d366cb2614eb55a94a42554bc165dcb0c9f604d8
SonMinSeock <44064257+SonMinSeock@users.noreply.github.com>
2023-10-17 17:41:56 +0900

    feat/slider 슬라이더 애니메이션 수정, 모바일 환경에서 슬라이더 할때 어색함 추후 변경 할 예정

--- client/src/components/Animation/Slider/Slider.tsx
+++ client/src/components/Animation/Slider/Slider.tsx
@@ -27,9 +27,8 @@ export default function Slider() {
       },
     },
     exit: (isBack: boolean) => ({
-      x: isBack ? 500 : -500,
       opacity: 0,
-      scale: 0,
+
       transition: {
         duration: 0.5,
       },
