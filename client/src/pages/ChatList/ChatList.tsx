2796eea158055b7a0155bf94db1868a3aff5ebe6
phj <aaagowns@gmail.com>
2023-10-25 21:18:44 +0900

    Design : 대화목록 디자인

--- client/src/pages/ChatList/ChatList.tsx
+++ client/src/pages/ChatList/ChatList.tsx
@@ -1,11 +1,18 @@
-import React from "react";
-import { useRecoilValue } from "recoil";
-import { isLoginAtom } from "../../recoil/login/atoms";
-
-const ChatList = () => {
-  const isLogin = useRecoilValue(isLoginAtom);
-  console.log(isLogin);
-  return <div>ChatList</div>;
-};
+import * as S from "./ChatList.style";
+const ChatList = () => (
+  <S.ChatListLayout>
+    <S.ChatListTitle>대화 목록</S.ChatListTitle>
+    <S.ChatList>
+      <S.ChatListItem>
+        <S.ChatListProfileImg src="https://avatars.githubusercontent.com/u/102671646?v=4" />
+        <S.ChatListInfoBox>
+          <S.ChatListUserName>박해준</S.ChatListUserName>
+          <S.ChatListMessages>여기에 최근 메시지</S.ChatListMessages>
+        </S.ChatListInfoBox>
+        <S.ChatListTime>오전 7:40</S.ChatListTime>
+      </S.ChatListItem>
+    </S.ChatList>
+  </S.ChatListLayout>
+);
 
 export default ChatList;
