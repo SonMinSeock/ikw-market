1c934faecbeea42797edd3f4cdef301b2ce513da
SonMinSeock <44064257+SonMinSeock@users.noreply.github.com>
2023-10-27 18:46:59 +0900

    feat/chat Chat 모델 스키마 제작

---
+++ server/models/chat.js
@@ -0,0 +1,34 @@
+import mongoose from "mongoose";
+const Schema = mongoose.Schema;
+
+const chatSchema = new Schema({
+  // 채팅방 타이틀.
+  // 채팅하기 버튼 클릭할시, 해당상품 이름으로 채팅방 타이틀로 생성한다.
+  title: {
+    type: String,
+    required: true,
+  },
+  // 채팅 보낸 기록들.
+  message_log: [
+    {
+      // 채팅 보낸 유저.
+      send_user: {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+      },
+      message: {
+        type: String,
+      },
+      // 채팅 한 날짜.
+      send_date: {
+        type: String,
+      },
+    },
+  ],
+  // 채팅방 만든 날짜.
+  created_at: {
+    type: Date,
+  },
+});
+
+export const Chat = mongoose.model("Chat", chatSchema);
