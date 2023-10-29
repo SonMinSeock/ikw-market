14890292733f9f1bf26139d40bb788f85a935ffc
SonMinSeock <44064257+SonMinSeock@users.noreply.github.com>
2023-10-29 19:19:53 +0900

    feat/chat chat api try-catch문으로 변경, 채팅 작성 할시 메시지 로그 기록해주는 API 제작

--- server/Router/chats.js
+++ server/Router/chats.js
@@ -7,48 +7,72 @@ import { currentDate } from "../lib/date";
 const router = express.Router();
 
 router.post("/:productId", async (req, res) => {
-  const sessionUser = req.session.user;
-  if (sessionUser) {
-    const { productId } = req.params;
-    const product = await Product.findById(productId).populate("seller_info");
+  try {
+    const sessionUser = req.session.user;
+    if (sessionUser) {
+      const { productId } = req.params;
+      const product = await Product.findById(productId).populate("seller_info");
 
-    const sellerUser = await User.findById(product.seller_info._id).populate("chat_room");
-    const user = await User.findOne({ social_id: sessionUser["social_id"] }).populate("chat_room");
+      const sellerUser = await User.findById(product.seller_info._id).populate("chat_room");
+      const user = await User.findOne({ social_id: sessionUser["social_id"] }).populate("chat_room");
 
-    const chatRoom = { title: product.product_name, created_at: currentDate() };
+      const chatRoom = { title: product.product_name, created_at: currentDate() };
 
-    let isChat = false;
+      let isChat = false;
 
-    // 판매자 채팅방 참여하고 있는지 확인해주는 부분.
-    for (let x = 0; x < sellerUser.chat_room.length; x++) {
-      for (let y = 0; y < user.chat_room.length; y++) {
-        if (sellerUser.chat_room[x].product.equals(user.chat_room[y].product)) {
-          isChat = true;
-          break;
+      // 판매자 채팅방 참여하고 있는지 확인해주는 부분.
+      for (let x = 0; x < sellerUser.chat_room.length; x++) {
+        for (let y = 0; y < user.chat_room.length; y++) {
+          if (sellerUser.chat_room[x].product.equals(user.chat_room[y].product)) {
+            isChat = true;
+            break;
+          }
         }
+        if (isChat) break;
       }
-      if (isChat) break;
+
+      if (!isChat) {
+        const createdChatRoom = new Chat({
+          title: chatRoom.title,
+          message_log: [],
+          seller_user: sellerUser,
+          product,
+          created_at: chatRoom.created_at,
+        });
+
+        sellerUser.chat_room.push(createdChatRoom);
+        user.chat_room.push(createdChatRoom);
+        await createdChatRoom.save();
+        await sellerUser.save();
+        await user.save();
+      }
+
+      res.json({ state: true });
+    } else {
+      res.json({ state: false });
     }
-
-    if (!isChat) {
-      const createdChatRoom = new Chat({
-        title: chatRoom.title,
-        message_log: [],
-        seller_user: sellerUser,
-        product,
-        created_at: chatRoom.created_at,
-      });
-
-      sellerUser.chat_room.push(createdChatRoom);
-      user.chat_room.push(createdChatRoom);
-      await createdChatRoom.save();
-      await sellerUser.save();
-      await user.save();
-    }
-
-    res.json({ state: true });
-  } else {
-    res.json({ state: false });
+  } catch (err) {
+    console.log("Error Created Chat API : ", err);
   }
 });
+
+router.post("/:productId/chat/:chatId", async (req, res) => {
+  try {
+    const {
+      params: { productId, chatId },
+      body: { message_log },
+    } = req; // message_log 배열 형식.
+
+    const chat = await Chat.findById(chatId);
+
+    chat.message_log.push(...message_log);
+
+    await chat.save();
+
+    res.json({ state: true });
+  } catch (err) {
+    console.log("Error Message Log Push API : ", err);
+  }
+});
+
 export default router;
