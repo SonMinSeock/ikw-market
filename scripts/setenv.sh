104050a42d1cc9fc798bafdbd5b281415f47b5bb
phj <aaagowns@gmail.com>
2023-11-28 17:24:03 +0900

    sh 수정

--- scripts/setenv.sh
+++ scripts/setenv.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-echo "MONGO_URI=$(aws ssm get-parameter --name MONGO_URI --query 'Parameter.Value' --output text)" >> server/.env
+echo "MONGO_URI=$(aws ssm get-parameter --name MONGO_URI --query 'Parameter.Value' --output text)" | sudo tee -a server/.env
+
