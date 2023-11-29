b61829e3cd16b958b309f88badd9895f5b1e55a0
phj <aaagowns@gmail.com>
2023-11-29 18:02:59 +0900

    aws 캐시 무효화 추가

--- client/src/pages/Main/Main.tsx
+++ client/src/pages/Main/Main.tsx
@@ -3,8 +3,6 @@ import * as S from "./Main.style";
 import ProductList from "./ProductList/ProductList";
 
 const Main = () => {
-  // ... (다른 환경 변수들도 추가)
-
   return (
     <S.MainLayout>
       <S.MainTitle>최근 중고거래 매물</S.MainTitle>
