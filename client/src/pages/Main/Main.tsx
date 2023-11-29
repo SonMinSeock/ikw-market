fee076bd95c8959cc9af555d9032128a5b7aee3a
phj <aaagowns@gmail.com>
2023-11-29 17:06:03 +0900

    환경변수 오류 수정

--- client/src/pages/Main/Main.tsx
+++ client/src/pages/Main/Main.tsx
@@ -3,6 +3,10 @@ import * as S from "./Main.style";
 import ProductList from "./ProductList/ProductList";
 
 const Main = () => {
+  console.log("REACT_APP_EXPRESS_URL:", process.env.REACT_APP_EXPRESS_URL);
+
+  // ... (다른 환경 변수들도 추가)
+
   return (
     <S.MainLayout>
       <S.MainTitle>최근 중고거래 매물</S.MainTitle>
