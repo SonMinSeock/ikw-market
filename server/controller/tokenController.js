8d1c1c7c85c0726b66045caca797d08a29c67a8e
phj <aaagowns@gmail.com>
2024-01-12 03:24:24 +0900

    token.js -> tokenController.js

--- server/controller/tokenController.js
+++ server/controller/tokenController.js
@@ -1,5 +1,5 @@
 import jwt from "jsonwebtoken";
-import { User } from "./models/user";
+import { User } from "../models/user";
 
 export const tokenCheckMiddleWare = (req, res, next) => {
   const accessToken = req.cookies.accessToken;
