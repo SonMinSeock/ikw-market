895b2933cf3c0c1dee48e2066a514b18414cd6c2
phj <aaagowns@gmail.com>
2024-01-25 01:37:11 +0900

    env 추가

--- scripts/setenv.sh
+++ scripts/setenv.sh
@@ -9,3 +9,7 @@ touch "$ENV_FILE"
 # 파일에 내용 추가
 echo "MONGO_URI=$(aws ssm get-parameter --name MONGO_URI --query 'Parameter.Value' --output text)" >> "$ENV_FILE"
 echo "JWT_SECRET_KEY=$(aws ssm get-parameter --name JWT_SECRET_KEY --query 'Parameter.Value' --output text)" >> "$ENV_FILE"
+echo "REDIS_HOST=$(aws ssm get-parameter --name REDIS_HOST --query 'Parameter.Value' --output text)" >> "$ENV_FILE"
+echo "REDIS_PORT=$(aws ssm get-parameter --name REDIS_PORT --query 'Parameter.Value' --output text)" >> "$ENV_FILE"
+echo "REDIS_USERNAME=$(aws ssm get-parameter --name REDIS_USERNAME --query 'Parameter.Value' --output text)" >> "$ENV_FILE"
+echo "REDIS_PASSWORD=$(aws ssm get-parameter --name REDIS_PASSWORD --query 'Parameter.Value' --output text)" >> "$ENV_FILE"
